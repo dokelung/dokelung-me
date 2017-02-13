30507ae7b07dbf60a91af06c7666d4a9fcd784bb
Ko-Lung Yuan <Ko-Lung Yuan>
2017-02-14 02:19:47 +0800

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'dokelung' # theme
+
+SITENAME = 'dokelung.me' # theme
+SITEURL = 'http://localhost:8000' # theme
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = 'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+DIRECT_TEMPLATES = ('index', 'categories', 'tags', 'archives', 'search')
+
+# path settings =============================================================
+PATH = 'content'
+
+PLUGIN_PATHS = ['../pelican-plugins']
+
+PLUGINS = ['tipue_search', 'just_table', "representative_image"]
+
+STATIC_PATHS = ['images', 'articles']
+
+ARTICLE_PATHS = ['articles']
+ARTICLE_URL = 'category/{category}/{slug}/'
+ARTICLE_SAVE_AS = 'category/{category}/{slug}/index.html'
+
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+
+THEME = '../jojo'
+
+# jojo theme settings =======================================================
+
+"""
+put all photos under content/images and just specify the base name of it in
+following settings
+"""
+
+# site settings
+SHORTCUT_ICON = 'dokelung.jpg'
+
+# right side panels
+SOCIAL = {
+    'style': {
+        'size': 'medium', # small, medium, large
+        'hover': True,    # True, False
+        'button': False,  # True, False
+    },
+    # for SOCIAL, jojo supports uk-icon in uikit2
+    # but jojo only recover following icons' color
+    'icons': (
+        ('envelope-square', 'dokelung@gmail.com'),
+        # ('facebook-square', '#'),
+        ('github-square', 'https://github.com/dokelung'),
+        # ('google-plus-square', '#'),
+        ('linkedin-square', 'https://www.linkedin.com/in/ko-lung-yuan-536b9aa3/'),
+        # ('skype', '#'),
+        # ('twitter-square', '#'),
+        # ('weixin', '#'),
+    )
+}
+AUTHOR_INFO = {
+    'id': AUTHOR,
+    'photo': 'dokelung.jpg',
+    'intro_keywords': (
+        ('a book author', '#'),
+        ('a pythoner', '#'),
+    ),
+    'intro': [
+        'Hi, my name is Ko-Lung Yuan!',
+        '你好，我是 袁克倫。'
+    ],
+    'url': os.path.join(SITEURL, 'pages', 'about-me'),
+    'social': SOCIAL,
+}
+
+NEWEST_ARTICLES = 10 # set 0 to hide this panel
+
+SIMPLE_PANELS = (
+    {
+        'badge': {
+            'string': 'book',
+            # type can be specified as '' or 'success' or 'warning' or 'danger'
+            # by default, '' is blue, 'success' is green, 'warning' is orange and 'danger' is red
+            # please reference to uikit2
+            'type': 'danger',
+        },
+        'title': "It's django",
+        'photo': 'its_django.jpg',
+        'content': 'A book about python web framework Django',
+        'link': ('Where to buy it?', '#'),
+    },
+)
+
+RELATED_LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+    ('mg', 'https://github.com/lucachr/pelican-mg'),
+)
+
+# left side buttons
+SHARE_BUTTONS = True
+CONTROL_BUTTONS = True
+
+# top
+NAV = {
+    'sitename': SITENAME,
+    'navitems': (
+        {
+            'primary': ('About me', AUTHOR_INFO['url']),
+        },
+        {
+            'primary': ('Category', os.path.join(SITEURL, 'categories.html')),
+            'secondary': (
+                {'type':'header', 'name':'Programming'},
+                {'link':('python', os.path.join(SITEURL, 'category', 'python.html')) },
+                {'type':'divider'},
+                {'link':('pelican', os.path.join(SITEURL, 'category', 'pelican.html')) },
+                # {'link':('misc', os.path.join(SITEURL, 'category', 'misc.html'))},
+            )
+        },
+        {
+            'primary': ('Archives', os.path.join(SITEURL, 'archives.html')),
+        },
+    ),
+    'tipue_search': True,
+}
+
+LOCATION = True
+
+# footer
+FOOTER = {
+    'year': 2017,
+    'author': AUTHOR,
+    'license': {
+        'name': 'The MIT License',
+        'link': 'https://opensource.org/licenses/MIT',
+    }
+}
+
+# just table settings
+JTABLE_TEMPLATE = """
+<table class="uk-table uk-table-striped">
+    {% if caption %}
+    <caption> {{ caption }} </caption>
+    {% endif %}
+    {% if th != 0 %}
+    <thead>
+    <tr>
+        {% if ai == 1 %}
+        <th> No. </th>
+        {% endif %}
+        {% for head in heads %}
+        <th>{{ head }}</th>
+        {% endfor %}
+    </tr>
+    </thead>
+    {% endif %}
+    <tbody>
+        {% for body in bodies %}
+        <tr>
+            {% if ai == 1 %}
+            <td> {{ loop.index }} </td>
+            {% endif %}
+            {% for entry in body %}
+            <td>{{ entry }}</td>
+            {% endfor %}
+        </tr>
+        {% endfor %}
+    </tbody>
+</table>
+"""
+
+DISQUS_SITENAME = "dokelung-me"
+DISQUS_CONFIG = True
